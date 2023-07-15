478304b327afcc5a4634109caa4338767d6067bc
Malgosia <malgosialeonowicz@gmail.com>
2023-07-15 12:19:57 +0200

    Display the forecast data using the API.

--- src/app.js
+++ src/app.js
@@ -14,33 +14,52 @@ function formatDate (timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000); 
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-function displayForecast() {
+    return days[day]; 
+
+}
+
+
+function displayForecast(response) {
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
     
     let forecastHTML = `<div class="row">`;
-    let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-    days.forEach(function (day) {
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
         forecastHTML = forecastHTML + 
         `<div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
             <img 
-                src="http://openweathermap.org/img/wn/01n@2x.png" 
+                src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
                 alt=""
                 width="42"
                 />
                 <div class="weather-forecast-temperatures">
-                    <span class="weather-forecast-temperature-max">18°</span>
-                    <span class="weather-forecast-temperature-min">12°</span>
+                    <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+                    <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
                 </div>
         </div>
         `;
+    }
 
     })
     
   
     forecastElement.innerHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates){
+ let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+ console.log(apiUrl);
+ axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -52,28 +71,24 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
-
-   
+    let iconElement = document.querySelector("#icon");
 
     celsiusTemperature = response.data.main.temp;
 
-
-    let iconElement = document.querySelector("#icon");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt*1000);
     iconElement.setAttribute(
         "src", 
-        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        );
  
-    iconElement.setAttribute(
-        "alt", response.data.weather[0].description);
-        
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 
-        temperatureElement.innerHTML = Math.round(celsiusTemperature);
-        cityElement.innerHTML = response.data.name;
-        descriptionElement.innerHTML = response.data.weather[0].description;
-        humidityElement.innerHTML = response.data.main.humidity;
-        windElement.innerHTML = Math.round(response.data.wind.speed);
-        dateElement.innerHTML = formatDate(response.data.dt*1000);
-
+    getForecast(response.data.coord);
     } 
 
 
@@ -119,4 +134,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
